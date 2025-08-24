387252f3689fd238117031f01d6d605196c72e92
Cisco <35647972+ciscoxwh@users.noreply.github.com>
2025-08-24 15:26:34 -0700

    the origination. everything was created at this moment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,76 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+export default function happyTime() {
+  const [bobs, setBobs] = useState(0); /* this makes a function, setBobs, that 
+  sets the value of the variable bobs */
+  const [bobsIncrement, setBobsIncrement] =
+    useState(1); /* this makes a function, setBobsIncrement, that 
+  sets the value of the variable bobsIncrement */
+  const [passportType, setPassportType] = useState(
+    "/thailandPassport.jpg"
+  ); /* this makes a function, setPassportType, that 
+  sets the value of the variable passportType*/
+
+  const [passportName, setPassportName] =
+    useState("Red Thai"); /* this makes a function, setPassportType, that 
+  sets the value of the variable passportType*/
+
+  const bobClick = () => {
+    const theNextValue = bobs + bobsIncrement;
+    // Predict the next value when we increment "bobs" by 1
+    // This value is called "theNextValue"
+    setBobs(theNextValue);
+
+    // sets bobs to the next value
+
+    if (theNextValue < 10) {
+      let help = "/thailandPassport.jpg";
+      setPassportName("Red Thai");
+      setPassportType(help);
+      setBobsIncrement(1);
+    } else if (theNextValue < 100) {
+      let help = "/awesomeThailandPassport.jpg";
+      setPassportName("Blue Thai");
+      setPassportType(help);
+      setBobsIncrement(3);
+    } else {
+      let help = "/awesomeBoblandiaPassport.png";
+      setPassportName("Boblandian");
+      setPassportType(help);
+      setBobsIncrement(10000000000);
+    }
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <h1>Bob's Thai/Boblandian customs.</h1>
+      <p>Hello. You have the {passportName} Passport.</p>
+      {/* please excuse all the br tags I will learn margins or whatever later */}
+      <br></br>
+      <button onClick={bobClick}>Click ME For +{bobsIncrement} Bobs</button>
+      <br></br>
+      <br></br>
+      <p>You Have {bobs} Bobs, Bro.</p>
+      <br></br>
+      <Image src={passportType} width={100} height={100} alt="{passportType}" />
+      <br></br>
+      <br></br>
+      <p>With 10 Bobs, you have the cool Blue Thailand passport.</p>
+      <p>
+        With 100 Bobs, you have the Boblandian passport (very powerful you can
+        travel anywhere)
+      </p>
+      <br></br>
+      <p>
+        Bob Sez: When you have the Blue Thailand passport, you get 3 bobs per
+        press instead.
+      </p>
+      <p>
+        Additionally, Bob Sez: When you have the Boblandian passport, you get 1e
+        <sup>10</sup> bobs per press instead.
+      </p>
     </div>
   );
 }
